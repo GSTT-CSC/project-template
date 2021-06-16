3183001361c33f45094a8753074c04c016d3a739
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2021-06-16 17:02:12 +0100

    First commit

--- train.py
+++ train.py
@@ -7,20 +7,23 @@ from torch.cuda import is_available as cuda_available
 
 def train(data_dir):
 
-    # initialise netowrk and datamodule
-    net = Network(data_dir=data_dir)
-    dm = DataModule(data_dir=data_dir, batch_size=4)
+    # initialise network and datamodule
+    print('Creating Network and DataModule')
+    dm = DataModule(data_dir=data_dir)
+    net = Network()
 
     #  start logged run
+    print('Starting logged run')
     mlflow.pytorch.autolog()
     with mlflow.start_run() as run:
         trainer = pl.Trainer(logger=True,
                              gpus=-1 if cuda_available() else None,
                              accelerator='ddp'
                              )
+        print('Starting training')
         trainer.fit(net, dm)
 
 
 if __name__ == '__main__':
-    data_dir = '/DATA/Task09_Spleen/'
+    data_dir = '/Users/lj16/code/Other/DATA/carnax/Carnax_Images_V2'
     train(data_dir)
