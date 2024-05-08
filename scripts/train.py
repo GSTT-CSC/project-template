67db3b30064776a48a3f95144818843bbc2a506b
mikewoodward94 <105747447+mikewoodward94@users.noreply.github.com>
2024-05-08 15:35:40 +0100

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -1,55 +1,152 @@
-import mlflow
-import pytorch_lightning as pl
 import sys
 import configparser
-from torch.cuda import is_available as cuda_available
-from pytorch_lightning.loggers import MLFlowLogger
 import logging
+import os
+import multiprocessing
+import json
+
+import mlflow
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from ray.air.integrations.mlflow import setup_mlflow
+from torch.cuda import is_available as cuda_available
+
 from project.DataModule import DataModule
 from project.Network import Network
+from project.DataModule import label_dict
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 
-def train(config, epochs=1):
-    # config
-    num_workers = 4
-    gpus = 4
-    test_batch = -1  # useful to test a small subset of data to ensure things are working, set to 0 to use all data
+logger = logging.getLogger(__name__)
 
-    # initialise network and datamodule
-    net = Network(data_dir=data_dir)
-    dm = DataModule(data_dir=data_dir, batch_size=4, test_batch=test_batch, num_workers=num_workers)
-
-    mlf_logger = MLFlowLogger(
-        experiment_name=config['project']['name'],
+def train(config):
+    # Set up mflow experiment
+    setup_mlflow(
+        tracking_uri=mlflow.get_tracking_uri(),
+        experiment_id=mlflow.get_experiment_by_name(
+            config["project"]["name"]
+        ).experiment_id
+        if mlflow.get_experiment_by_name(config["project"]["name"])
+        else mlflow.create_experiment(config["project"]["name"]),
     )
 
-    #  start logged run-
-    mlflow.pytorch.autolog()
-    with mlflow.start_run() as run:
-        trainer = pl.Trainer(
-            logger=mlf_logger,
-            auto_select_gpus=True,
-            precision=16 if cuda_available() else 32,
-            devices=gpus if cuda_available() else None,
-            accelerator='gpu' if cuda_available() else None,
-            log_every_n_steps=1,
-            max_epochs=epochs,
-            strategy="ddp" if cuda_available() else None,
-            num_sanity_val_steps=0,
-        )
-        trainer.fit(net, dm)
+    # Set up variables from config
+    xnat_configuration = {'server': config['xnat']['SERVER'],
+                        'user': config['xnat']['USER'],
+                        'password': config['xnat']['PASSWORD'],
+                        'project': config['xnat']['PROJECT'],
+                        'verify': config.getboolean('xnat', 'VERIFY')}
 
-    logging.info('Training complete')
+    max_workers = int(config['params']['MAX_WORKERS'])
+    num_workers = (
+        max_workers
+        if max_workers < multiprocessing.cpu_count()
+        else multiprocessing.cpu_count()
+    )
+       
+    save_best_model = True
+
+    mlflow.pytorch.autolog(log_models=False)
+
+    # Set up datamodule
+    dm = DataModule(
+        xnat_configuration = xnat_configuration,
+        num_workers = num_workers,
+        batch_size = int(config['params']['batch_size']),
+        )
+
+    dm.prepare_data()
+
+    n_classes = len(set([x for x in label_dict.values() if x is not None]))
+    mlflow.log_param('n_classes', n_classes)
+
+    train_class_weights = dm.data_manifest["train"]["class_weights"]
+    validation_class_weights = dm.data_manifest["validation"]["class_weights"]
+
+    # Set up network
+    net = Network(
+        # Include params here from config such as dropout, learning_rate as shown
+        n_classes = n_classes,
+        train_class_weights = train_class_weights,
+        validation_class_weights = validation_class_weights,
+        dropout = float(config['params']['dropout']),
+        learning_rate = float(config['params']['learning_rate']),
+    )
+
+    # Callbacks
+    callbacks = []
+    callbacks.append(LearningRateMonitor(logging_interval="step"))
+    checkpoint_callback = ModelCheckpoint(
+        save_top_k=1,
+        monitor="val_loss",
+        mode="min",
+        dirpath="./checkpoint/",
+    )
+    callbacks.append(checkpoint_callback)
+
+    # configure trainer
+    trainer = pl.Trainer(
+        precision="16-mixed" if cuda_available() else "32-true",
+        callbacks=callbacks,
+        devices= "gpu" if cuda_available() else "auto",
+        accelerator="gpu" if cuda_available() else "auto",
+        log_every_n_steps=1,
+        max_epochs=int(config['params']['max_epochs']),
+    )
+    
+    trainer.fit(net, dm)
+
+    logging.info("Training complete: running test set")
+
+    # Create and log classification report
+    if save_best_model:
+        net_trained = Network.load_from_checkpoint(
+            checkpoint_callback.best_model_path,
+            n_classes = n_classes,
+            train_class_weights = train_class_weights,
+            validation_class_weights = validation_class_weights,
+            dropout = float(config['params']['dropout']),
+            learning_rate = float(config['params']['learning_rate']),
+            )
+        trainer.test(
+            net_trained,
+            datamodule=dm,
+        )
+        checkpoint = torch.load(checkpoint_callback.best_model_path)
+
+        file_path = (
+            f"model-{config['project']['name']}-{mlflow.active_run().info.run_name}.pt"
+        )
+
+        checkpoint_info = {}
+        checkpoint_info["monitored_metric"] = checkpoint_callback.monitor
+        checkpoint_info["metric_value"] = checkpoint_callback.best_model_score.item()
+        checkpoint_info["mode"] = checkpoint_callback.mode
+        checkpoint_info["epoch"] = checkpoint["epoch"]
+
+        with open("checkpoint_info.json", "w") as f:
+            json.dump(checkpoint_info, f, indent=2)
+
+        script = torch.jit.script(net_trained._model)
+
+        mlflow.pytorch.log_model(
+            script, file_path, extra_files=["checkpoint_info.json"]
+        )
+
+        _ = [
+            os.remove(fp) for fp in ["checkpoint_info.json"]
+        ]  # remove the temporary files after logging to mlflow
+
 
 if __name__ == '__main__':
 
     if len(sys.argv) > 0:
         config_path = sys.argv[1]
     else:
-        config_path = 'config/config.cfg'
+        config_path = 'config/local_config.cfg'
 
     config = configparser.ConfigParser()
     config.read(config_path)
-    data_dir = config['data']['DATA_PATH']
     train(config)
 
